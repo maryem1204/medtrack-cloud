2127de556b252078ac9279c05f754d0c2baf4cd2
maryem1204 <maryemsassi.dev@gmail.com>
2026-07-18 19:32:36 +0100

    Ajout du squelette Spring Boot avec connexion MySQL

---
+++ backend/src/main/java/medtrack_backend/MedtrackBackendApplication.java
@@ -0,0 +1,13 @@
+package medtrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedtrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedtrackBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/medtrack_backend/MedtrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package medtrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedtrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
